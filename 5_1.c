21c893cf22f6db3eee437e947be459ade3a97c14
EarthEngineer <101568875+EarthEngineer@users.noreply.github.com>
2022-10-04 20:44:07 +0300

    Update 5_1.c

--- 5_1.c
+++ 5_1.c
@@ -3,18 +3,18 @@
 
 int main(){
 
-int n;
-scanf("%i", &n);
+    int n;
+    scanf("%i", &n);
 
-long double result = 0.0;
-for(int i=1; i<=n; ++i){
-    long double t=0.0;
-    t+=pow(-1, i+1);
-    t/=(2*i)-1;
-    result+=t;
-}
-result*=4;
+    long double result = 0.0;
+    for(int i=1; i<=n; ++i){
+        long double t=0.0;
+        t+=pow(-1, i+1);
+        t/=(2*i)-1;
+        result+=t;
+    }
+    result*=4;
 
 
-printf("%Lf\n", result);
+    printf("%Lf\n", result);
 }
