cb020ea5c9e28e6e963b86dbc766c40f80762728
EarthEngineer <101568875+EarthEngineer@users.noreply.github.com>
2022-10-04 20:39:57 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,14 +2,13 @@
 
 unsigned long long fact(int  n)
 {
-unsigned long long result = 1;
-for (int i = 1; i <= n; ++i)
-    result *= i;
-return result;
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; ++i)
+        result *= i;
+    return result;
 }
-int main()
-{
-int k;
-scanf("%i", &k);
-printf("%llu\n", fact(k));
+int main(){
+    int k;
+    scanf("%i", &k);
+    printf("%llu\n", fact(k));
 }
