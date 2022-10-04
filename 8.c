9eb52258609bd8c8e24a86df44b733b9a5fad859
EarthEngineer <101568875+EarthEngineer@users.noreply.github.com>
2022-10-04 19:08:38 +0300

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+double length(double x, double y) {
+    return distance(x, y, 0, 0);
+}
+double scalar_product(double x1, double y1, double x2, double y2) {
+    return x1 * x2 + y1 * y2;
+}
+const double pi = 3.14159265359;
+const double eps = 1e-10;
+double to_degrees(double rad) {
+    return rad * 180 / pi;
+}
+double f(double x){
+    double f;
+    f=x*x*x*x*x+2*x*x*x*x+5*x*x+4*x-500;
+    return f;
+}
+
+int main(){
+    double l=0;
+    double h=10;
+    while(h-l>1e-10){
+        if (f((l+h)/2)>=0)
+            h=(l+h)/2;
+        else{ l=(l+h)/2;}
+    }
+    printf("%lf", (l+h)/2);
+}
