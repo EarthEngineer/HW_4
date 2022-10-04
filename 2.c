d9906ef4f3c4eeb105ded88f0658b52275d570ae
EarthEngineer <101568875+EarthEngineer@users.noreply.github.com>
2022-10-04 20:41:41 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 
-unsigned long long fact(int  n, int k)
-{
-unsigned long long result = 1;
-for (int i = k; i <= n; ++i)
-    result *= i;
-return result;
+unsigned long long fact(int  n, int k){
+
+    unsigned long long result = 1;
+    for (int i = k; i <= n; ++i)
+        result *= i;
+    return result;
 }
-int main()
-{
-int k, n;
-scanf("%i %i", &n, &k);
-printf("%llu\n", fact(n, n-k+1 ));
+int main(){
+
+    int k, n;
+    scanf("%i %i", &n, &k);
+    printf("%llu\n", fact(n, n-k+1 ));
 }
