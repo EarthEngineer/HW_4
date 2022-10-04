a7a74dc96ed11d0433c2552bec22292045cd4334
EarthEngineer <101568875+EarthEngineer@users.noreply.github.com>
2022-10-04 20:48:08 +0300

    Update 7.c

--- 7.c
+++ 7.c
@@ -2,17 +2,17 @@
 #include <math.h>
 
 double distance(double x1, double y1, double x2, double y2) {
-    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+      return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 double length(double x, double y) {
-    return distance(x, y, 0, 0);
+      return distance(x, y, 0, 0);
 }
 double scalar_product(double x1, double y1, double x2, double y2) {
-    return x1 * x2 + y1 * y2;
+      return x1 * x2 + y1 * y2;
 }
 const double pi = 3.14159265359;
 double to_degrees(double rad) {
-    return rad * 180 / pi;
+     return rad * 180 / pi;
 }
 
 int main(){
